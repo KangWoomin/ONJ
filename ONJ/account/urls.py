42e95264f7818f2df14367c59ca151207df21fca
KangWoomin <blacksu25@gmail.com>
2024-09-01 21:49:57 +0900

    create_user

--- ONJ/account/urls.py
+++ ONJ/account/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', view=main, name='main'),
     path('map/', view=map, name='map'),
+    path('create_user/', view=create_user, name='create_user'),
 ]
--- ONJ/account/views.py
+++ ONJ/account/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-
+from .forms import *
 # Create your views here.
 from django.http import HttpResponse
+
 
 def main(request):
     return render(request,'main.html')
@@ -12,3 +13,17 @@ def map(request):
         'naver':settings.NAVER_MAP_CLIENT_ID
     }
     return render(request,'map.html', content)
+
+
+
+from django.contrib.auth import login,logout
+def create_user(request):
+    if request.method == 'POST':
+        form = UserCreateForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('main')
+    else:
+        form = UserCreateForm()
+    return render(request,'create_user.html',{'form':form})
