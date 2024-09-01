6ea0ce01936566d0d8e611ac7f3c982bb5f68ed5
KangWoomin <blacksu25@gmail.com>
2024-09-01 18:50:06 +0900

    mainpage(요약)

--- ONJ/account/views.py
+++ ONJ/account/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def main(request):
-    return render(request,'base.html')
+    return render(request,'main.html')
 
 from django.conf import settings
 def map(request):
