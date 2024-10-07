7ffcc2afcd6645690d8fb4505e3ae7a6076b28d5
blacksu25 <blacksu25@gmail.com>
2024-10-07 16:44:19 +0900

    Update urls.py

--- ONJ/account/urls.py
+++ ONJ/account/urls.py
@@ -3,7 +3,10 @@ from .views import *
 
 urlpatterns = [
     path('', view=main, name='main'),
+
+    path('map_view/', view=map_view, name='map_view'),
     path('map/', view=map, name='map'),
+    
     path('create_user/', view=create_user, name='create_user'),
     path('login/', view= user_login, name='login'),
     path('logout/', view=user_logout, name='logout'),
